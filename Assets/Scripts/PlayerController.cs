6638db57f617f53cbaba2b81e148db5857a9b978
Gabin <gabin.3ds@hotmail.fr>
2021-01-05 00:09:05 +0100

    Trying to make the Player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
+
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public float moveSpeed = 2.0f;
+    public float jumpForce = 1.0f;
+    public float gravityValue = -9.81f;
+
+    private void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
+     {
+       
+     //   groundedPlayer = Physics.CheckSphere(new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), 0.4f, groundedPlayer);
+
+        //moving
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float y = Input.GetAxisRaw("Vertical") * moveSpeed;
+
+        //jumping
+     //   if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+     //       rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+
+      
+     //   Vector3 move = transform.right * x + transform.forward * y;
+
+     //   rb.velocity = new Vector3(move.x, rb.velocity.y,move.z);
     }
 }
